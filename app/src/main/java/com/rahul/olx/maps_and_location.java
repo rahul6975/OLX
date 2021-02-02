52fb4154441362ea9e32573db3c456451c279695
DineshReddyP <40158666+DineshReddyP@users.noreply.github.com>
2021-02-02 20:10:39 +0530

    pulling code

---
+++ app/src/main/java/com/rahul/olx/maps_and_location.java
@@ -0,0 +1,14 @@
+package com.rahul.olx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class maps_and_location extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_and_location);
+    }
+}
