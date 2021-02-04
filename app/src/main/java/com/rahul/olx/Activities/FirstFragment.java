5aadee3e383551710146ba721102b000f9a8d0b9
Dhanashree Rane <dhanashreerane18@gmail.com>
2021-02-05 00:35:48 +0530

    pull

--- app/src/main/java/com/rahul/olx/Activities/FirstFragment.java
+++ app/src/main/java/com/rahul/olx/Activities/FirstFragment.java
@@ -9,13 +9,16 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.rahul.olx.R;
+
 public class FirstFragment extends Fragment {
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
-    ) {
+    )
+    {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
--- app/src/main/java/com/rahul/olx/Activities/SecondFragment.java
+++ app/src/main/java/com/rahul/olx/Activities/SecondFragment.java
@@ -9,6 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.rahul.olx.R;
+
 public class SecondFragment extends Fragment {
 
     @Override
