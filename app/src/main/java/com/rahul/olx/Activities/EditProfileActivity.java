02ac0f3c113e2e5544df1980ea3dc5cac3b557d6
Dhanashree Rane <dhanashreerane18@gmail.com>
2021-02-05 11:19:52 +0530

    pulling code

---
+++ app/src/main/java/com/rahul/olx/Activities/EditProfileActivity.java
@@ -0,0 +1,14 @@
+package com.rahul.olx.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
