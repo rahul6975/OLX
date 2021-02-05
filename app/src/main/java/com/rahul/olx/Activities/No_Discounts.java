44102a88a38813d442a223ee770966285667bd90
DineshReddyP <40158666+DineshReddyP@users.noreply.github.com>
2021-02-05 18:12:36 +0530

    added discount search, discount and team layouts

---
+++ app/src/main/java/com/rahul/olx/Activities/No_Discounts.java
@@ -0,0 +1,16 @@
+package com.rahul.olx.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rahul.olx.R;
+
+public class No_Discounts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no__discounts);
+    }
+}
