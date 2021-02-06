368bc05b407f620bf143773cc150bd0694187c52
rahul6975 <rahulya7569@gmail.com>
2021-02-06 12:10:51 +0530

    done

--- app/src/main/java/com/rahul/olx/Activities/Help_and_Support.java
+++ app/src/main/java/com/rahul/olx/Activities/Help_and_Support.java
@@ -2,15 +2,29 @@ package com.rahul.olx.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.rahul.olx.R;
 
 public class Help_and_Support extends AppCompatActivity {
 
+    private TextView tvHep;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_and__support);
+        tvHep = findViewById(R.id.TvVersion);
+
+        tvHep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Help_and_Support.this,Team_Ivory.class);
+                startActivity(intent);
+            }
+        });
     }
 }
